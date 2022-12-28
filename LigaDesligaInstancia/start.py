61a961a574ed3741ae36b2ccf30f0971499450b5
Alexandre lo bianco dos santos <devlobianco@gmail.com>
2022-12-28 13:06:37 -0300

    projeto ok

--- LigaDesligaInstancia/start.py
+++ LigaDesligaInstancia/start.py
@@ -1,6 +1,6 @@
 import boto3
 region = 'us-east-1'
-instances = ['i-0d50787ad72398073']
+instances = ['i-0b98a71157caba8f8']
 ec2 = boto3.client('ec2', region_name=region)
 
 def lambda_handler(event, context):
